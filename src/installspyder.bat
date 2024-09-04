c6a8d1a847b2fc9a09ff00eb21c94703bedc3a1a
lendres <lanceendres@gmail.com>
2024-09-04 06:49:00 -0500

    Update installspyder.bat

--- src/installspyder.bat
+++ src/installspyder.bat
@@ -9,11 +9,19 @@ IF "%1"=="-h" (
     echo Install Spyder from the standalone download.
     echo.
     echo SPYDERINSTALL [arg1]
-    echo     arg1 : name of the environment
+    echo     arg1   Name of the environment.  If provided Sypder will be installed into this environment,
+    echo            If not provided, Spyder will be installed into the currently active environment.
     GOTO :EOF
 )
 
-call activateenv %1
+IF "%1" NEQ "" (
+    echo Installing in environment: %1
+    call activateenv %1
+) ELSE (
+   echo No environment provided, installing into the currently activate environment.
+)
+echo.
+
 call pip install spyder numpy scipy pandas matplotlib sympy cython
 
 :EOF
